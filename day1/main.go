60b3ce69ef94c890f08b9fd80ac915b099bac535
Adz-ai <adarssh@live.co.uk>
2024-11-30 20:09:27 +0000

    preprep for day1

---
+++ day1/main.go
@@ -0,0 +1,17 @@
+package day1
+
+import "fmt"
+
+func Part1(input []string) int {
+	return 0
+}
+
+func Part2(input []string) int {
+	return 0
+}
+
+func main() {
+	input := ParseTextFile("input.txt")
+	fmt.Println(Part1(input))
+	fmt.Println(Part2(input))
+}
