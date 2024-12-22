ebdd98ef18034bf2c7d5a1637cd042f1964f6b28
Adarssh Athithan <adarssh@live.co.uk>
2024-12-22 11:33:16 +0000

    wip day22

--- day22/main.go
+++ day22/main.go
@@ -6,57 +6,105 @@ import (
 	"strconv"
 )
 
-const (
-	MOD = 16777216
-)
-
-// mix performs a bitwise XOR operation between the secret and value
-func mix(secret, value int) int {
-	return secret ^ value
+type bananaPrice struct {
+	num    int
+	change int
 }
 
-// prune performs modulo operation with MOD constant
-func prune(secret int) int {
-	return secret % MOD
+type seq struct {
+	a, b, c, d int
 }
 
-// generateNext creates the next secret number based on the current one
-func generateNext(secret int) int {
-	result := mix(secret, secret*64)
-	result = prune(result)
-
-	result = mix(result, result/32)
-	result = prune(result)
-
-	result = mix(result, result*2048)
-	result = prune(result)
-
-	return result
-}
-
-// generate2000th generates the 2000th number in the sequence
-func generate2000th(initial int) int {
-	current := initial
-	for i := 0; i < 2000; i++ {
-		current = generateNext(current)
+// Get price and change from a secret number
+func getNumAndChange(input, previous int) bananaPrice {
+	currentNum := input % 10
+	return bananaPrice{
+		num:    currentNum,
+		change: currentNum - previous,
 	}
-	return current
 }
 
-func part1(input []string) int {
-	var sum int
+func mix(value, secretNum int) int {
+	return value ^ secretNum
+}
 
-	for _, line := range input {
-		initial, err := strconv.Atoi(line)
-		if err != nil {
-			log.Fatalf("Error parsing input: %v\n", err)
+func prune(value int) int {
+	return value % 16777216
+}
+
+func findSecretNumber(input int) int {
+	input = prune(mix(64*input, input))
+	input = prune(mix(input/32, input))
+	input = prune(mix(input*2048, input))
+	return input
+}
+
+func findMaxNumberOfBananas(b [][]bananaPrice) int {
+	seqMap := make(map[seq][]int)
+
+	// For each buyer
+	for i := range b {
+		// Initial sliding window of changes
+		s := []int{b[i][0].change, b[i][1].change, b[i][2].change}
+
+		// Slide window through all changes
+		for j := 3; j < len(b[i]); j++ {
+			s = append(s, b[i][j].change)
+			sequence := seq{s[0], s[1], s[2], s[3]}
+
+			// Initialize sequence in map if not exists
+			if _, ok := seqMap[sequence]; !ok {
+				seqMap[sequence] = make([]int, len(b))
+			}
+
+			// Store first occurrence of price for this buyer
+			if seqMap[sequence][i] == 0 {
+				seqMap[sequence][i] = b[i][j].num
+			}
+
+			// Move window forward
+			s = s[1:]
 		}
-
-		final := generate2000th(initial)
-		sum += final
 	}
 
-	return sum
+	// Find sequence that gives maximum bananas
+	maxBananas := 0
+	for _, prices := range seqMap {
+		sum := 0
+		for _, price := range prices {
+			sum += price
+		}
+		if sum > maxBananas {
+			maxBananas = sum
+		}
+	}
+
+	return maxBananas
+}
+
+func solve(input []string) (int, int) {
+	var sum int
+	priceData := make([][]bananaPrice, len(input))
+
+	// Process each buyer
+	for i, line := range input {
+		num, _ := strconv.Atoi(line)
+		prev := num % 10 // Initial price
+
+		// Generate 2000 numbers and their changes
+		priceData[i] = make([]bananaPrice, 2000)
+		for j := 0; j < 2000; j++ {
+			num = findSecretNumber(num)
+			priceData[i][j] = getNumAndChange(num, prev)
+			prev = num % 10
+
+			if j == 1999 { // Last number for part 1
+				sum += num
+			}
+		}
+	}
+
+	return sum, findMaxNumberOfBananas(priceData)
 }
 
 func main() {
@@ -64,5 +112,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(part1(input))
+	part1, part2 := solve(input)
+	log.Printf("Part 1: %d\n", part1)
+	log.Printf("Part 2: %d\n", part2)
 }
